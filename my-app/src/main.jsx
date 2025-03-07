3cf2224a8a888c2f5aa952763369f284ad088600
IoannaDamianov <damianov.io@northeastern.edu>
2025-03-07 12:46:19 -0500

    added styling and consolidated files

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './front-end/index.css'
+import App from './front-end/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
